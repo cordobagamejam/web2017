24eb884f401d2fca1e52e88a15088f00a2c1b1c0
Ignacio Castro <ignacio@venzee.com>
2016-01-05 00:20:23 -0300

    add controls

---
+++ public/app/controls.js
@@ -0,0 +1,64 @@
+/**
+ * Init controls
+ * @param user
+ * @returns {boolean}
+ */
+
+function controls (user) {
+
+    if (!user) { return false; }
+
+    // add touch controls
+
+    var element = document.getElementById('header-canvas');
+    var mc = Hammer(element);
+
+    mc.on(CGJ.controls.touch.RUN, checkPress);
+    mc.on(CGJ.controls.touch.JUMP, checkPressUp);
+
+    function checkPress (e) {
+        user.run(true, !user.velocity.fast);
+    }
+
+    function checkPressUp (e) {
+        user.run(true, false);
+        user.DoJump();
+    }
+
+
+    //bind keyboard controls
+    document.onkeydown = checkKey;
+    document.onkeyup = checkKeyUp;
+
+
+    function checkKeyUp (e) {
+        e = e || window.event;
+        if (e.keyCode == CGJ.controls.keyboard.RUN.key1 || e.keyCode == CGJ.controls.keyboard.RUN.key2) {
+            // right arrow
+            user.run(true, false);
+        }
+    }
+
+    function checkKey(e) {
+        e = e || window.event;
+
+        if (e.keyCode == CGJ.controls.keyboard.JUMP.key1 || e.keyCode == CGJ.controls.keyboard.JUMP.key2) {
+            // up arrow
+            user.run(true, user.velocity.fast);
+            user.DoJump();
+        }
+        else if (e.keyCode == '37') {
+            // left arrow
+            user.stop();
+        }
+        else if (e.keyCode == CGJ.controls.keyboard.RUN.key1 || e.keyCode == CGJ.controls.keyboard.RUN.key2) {
+            // right arrow
+            user.run(true, true);
+        }
+
+    }
+
+
+}
+
+
