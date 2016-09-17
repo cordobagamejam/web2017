af01e8a637556492d7ab349aeeb6326aadc0640a
ignacio castro <ignacio@castro.com>
2016-09-17 19:57:41 -0300

    add doloop method

--- public/app/custom/renderer.js
+++ public/app/custom/renderer.js
@@ -1,5 +1,5 @@
 
-function ntRenderer (name, w , h) {
+function gjRenderer (name, w , h) {
     var self = this;
 
     if(!name) {
@@ -13,18 +13,22 @@ function ntRenderer (name, w , h) {
 
     self.instance   = PIXI.autoDetectRenderer( self.width , self.height,  { transparent: true, view: document.getElementById(self.name) });
     self.stage      = new PIXI.Container();
-
+    self.deltaTime = {
+        previous: 0,
+        frameDuration: 1000 / this.fps,
+        lag : 0
+    };
     self.onResize();
 }
 
 //set fps at 60
-ntRenderer.prototype.fps = 60;
+gjRenderer.prototype.fps = 60;
 
-ntRenderer.prototype.minHeight = 500;
-ntRenderer.prototype.minWidth = 1000;
+gjRenderer.prototype.minHeight = 500;
+gjRenderer.prototype.minWidth = 1000;
 
 
-ntRenderer.prototype.onResize = function(w, h) {
+gjRenderer.prototype.onResize = function(w, h) {
     var self = this;
 
     window.onresize = function() {
@@ -34,7 +38,36 @@ ntRenderer.prototype.onResize = function(w, h) {
     };
 };
 
-ntRenderer.prototype.render = function () {
+gjRenderer.prototype.doLoop = function(callback, timestamp) {
+
+    var self = this;
+
+    window.requestAnimationFrame(
+        function(timestamp) {
+            self.doLoop(callback, timestamp)
+    });
+
+    if (!timestamp) { timestamp = 0; }
+    var elapsed = timestamp - self.deltaTime.previous;
+    if (elapsed > 1000) { elapsed = self.deltaTime.frameDuration };
+    self.deltaTime.lag += elapsed;
+
+    while (self.deltaTime.lag >= self.deltaTime.frameDuration) {
+
+        if(callback) {
+            callback();
+        }
+
+        self.deltaTime.lag -= self.deltaTime.frameDuration;
+    }
+
+
+    self.render();
+    self.deltaTime.previous = timestamp;
+
+};
+
+gjRenderer.prototype.render = function () {
     var self = this;
     self.instance.render(self.stage);
 };
