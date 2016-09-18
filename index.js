17372566922a4069c9e63d7c3b5131a67effef62
Juan Manuel Aleman <juale86@gmail.com>
2016-09-17 22:47:59 -0300

    Added db module for insert and get scores

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 var realtime = require('./server/realtime');
+var db_service = require('./server/db');
+var bodyParser = require('body-parser')
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -12,10 +14,35 @@ server.listen(port, function () {
 
 // Routing
 app.use(express.static(__dirname + '/dist'));
-
+app.use(bodyParser.json());
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/dist/index.html');
 });
 
 var numUsers = 0;
 realtime(io, numUsers);
+
+
+//on try to check score, return scored array
+app.get('/score', function(req, res){
+	db_service.getScore(function(results) {
+  		res.send(results);
+	});
+});
+
+
+
+//create new score
+app.post('/score', function(req, res){
+    //check if username exist
+    if(req.body.name){
+        //create and response new results
+        db_service.setScore(req.body.name, req.body.score || 0, function(docs){
+            res.send(docs);
+        });
+    }
+    else {
+        //reject DB document creation
+        res.status(400).send('Error, se necesita nombre: {"name": "YOUR_NAME", "score": Number("YOUR_SCORE")}');
+    }
+});
---
+++ server/db.js
@@ -0,0 +1,57 @@
+var mongo = require('mongodb').MongoClient;
+var db_name = 'mongodb://localhost:27017/cgj';
+var LIMIT = 10;
+
+var db_service = {
+	getScore: _getScore,
+	setScore: _setScore
+};
+
+
+function _setScore(param_name, param_value, callback){
+
+//form saved obj
+    var val = {
+        name : param_name, 
+        score : param_value
+    };
+
+//connect to the db
+    mongo.connect(db_name, function(err, db){
+        //use score collection
+        var col = db.collection('score');
+
+        //insert new document
+        col.insertOne(val, function(err, result){
+            //on document creation, response the new score list
+            col.find().sort({score: -1}).limit(LIMIT).toArray(function(err, docs){
+            	if(typeof callback === 'function') {
+                	callback(docs);
+				}
+            });
+            //close db
+            db.close();
+        });
+    });
+}
+
+function _getScore(callback) {
+    //mongo connect
+	mongo.connect(db_name, function(err, db){
+        //use score collection
+		var col = db.collection('score');
+	    //find 10 top users sorted by score 
+	    col.find().sort({score: -1}).limit(LIMIT).toArray(function(err, docs) {
+	    	//check if callback is a function to return async result
+            if(typeof callback === 'function') {
+                callback(docs);
+            }
+	    });
+        //wait db executions and close after it
+		db.close();
+	});
+}
+
+
+module.exports = db_service;
+
